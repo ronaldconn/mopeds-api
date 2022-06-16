59171fc9bba97a8a47d155e556ccc4b818e59e24
ronaldconn <97855104+ronaldconn@users.noreply.github.com>
2022-06-15 22:05:00 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const mopeds = {
+    'puch': {
+        'model': ['maxi', 'magnum', 'magnum-x', 'dart'],
+        'yearsProduced': '1974 1986',
+        'colors': ['yellow', 'apple-green', 'red', 'white' ],
+        'manufacturedLocation': 'Graz, Austria'
+    },
+    'minarelli': {
+        'model': [ 'Cimatti', 'Gitane', 'Testi', 'Concord', 'Motron', 'Fantic', 'General', 'AMF', 'Baretta', 'Gadabout', 'Motomarina', 'Safari'],
+        'yearsProduced': '1969 to 1986',
+        'colors': ['yellow', 'apple-green', 'red', 'white' ],
+        'manufacturedLocation': 'Italy'
+    },
+    'unknown':{
+        'model': 'unknown',
+        'yearsProduced': 0,
+        'colors': 'unknown',
+        'manufacturedLocation': 'unknown'
+}     
+}  
+
+app.get('/', (request, response)=>{
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name', (request, response)=>{
+    const mopedName = request.params.name.toLowerCase()
+    if ( mopeds[mopedName] ){
+        response.json( mopeds[mopedName])
+    }else{
+        response.json( mopeds['unknown'])
+    }
+    
+})
+
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log(`The server is now running on port ${PORT}! Betta Go Catch It!`)
+})
